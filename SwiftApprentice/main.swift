07003ebdacdb5983cac583345cf851019cd500ae
Fernanda Fulcherberguer <ffulcherberguer@lcs.on.ca>
2019-11-22 14:33:32 -0500

    Initial Commit

---
+++ SwiftApprentice/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftApprentice
+//
+//  Created by Fulcherberguer, Fernanda on 2019-11-22.
+//  Copyright © 2019 Fulcherberguer, Fernanda. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
